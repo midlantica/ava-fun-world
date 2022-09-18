aff230579aa7bcf563f33a956556248760f50721
Drew Harper <druharper@gmail.com>
2022-09-17 23:23:32 -0500

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
+// @ts-ignore
 import { astroImageTools } from "astro-imagetools";
-
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
+// export default ({
+//   integrations: [
+//     tailwind(),
+//     astroImageTools
+//   ]
+// })
+
+
+export default {
   integrations: [
     tailwind(),
     astroImageTools
-  ]
-});
+  ],
+};
